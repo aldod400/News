66c6bddcd5af06aac3edbe8cb753094ff0e38808
Mohamed Osama <m.osama2798@gmail.com>
2025-08-21 15:05:02 +0300

    fix: Update copyright link to point to the correct Marketopia Systems website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -409,7 +409,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    <span class="text-light"><a href="team.com"><i
+                    <span class="text-light"><a href="https://marketopiasystems.com/"><i
                                 class="fas fa-copyright text-light me-2"></i>MarketopiaTeam</a>, All right reserved.</span>
                 </div>
             </div>
